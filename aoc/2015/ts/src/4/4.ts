76d04cb86c9bf4bdcd33e521525cd46f19660ada
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-12-28 18:05:11 -0500

    Complete AOC 2015 Day 4.2 - Typescript

--- aoc/2015/ts/src/4/4.ts
+++ aoc/2015/ts/src/4/4.ts
@@ -1,9 +1,9 @@
 import * as crypto from 'crypto'
 
-// mineMD5: -> number
+// mineMD5ForFive: -> number
 // Purpose: Produces the first index where md5(puzzle + index) results in a hash
 //          with five zeros, in base 16
-const mineMD5 = (): number => {
+const mineMD5ForFive = (): number => {
   const prefix = 'yzbqklnj'
   let i = 0
   
@@ -17,10 +17,30 @@ const mineMD5 = (): number => {
   }
 }
 
+// mineMD5ForSix: -> number
+// Purpose: Produces the first index where md5(puzzle + index) results in a hash
+//          with six zeros, in base 16
+const mineMD5ForSix = (): number => {
+  const prefix = 'yzbqklnj'
+  let i = 0
+  
+  while (true) {
+    const hash =  crypto.createHash('md5').update(`${prefix}${i}`).digest('hex')
+    if (hash.substring(0, 6) === '000000') {
+      return i
+    }
+
+    i += 1
+  }
+}
+
 
 const stars = () => {
-  const output = mineMD5()
-  console.log(output)
+  const outputOne = mineMD5ForFive()
+  const outputTwo = mineMD5ForSix()
+
+  console.log(outputOne)
+  console.log(outputTwo)
 }
 
 const tests = () => {
