bda6bf971487c174fa4739a53dc526b1ea91d536
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-12-28 15:25:27 -0500

    Complete AOC 2015 Day 1.2 - Typescript

--- aoc/2015/ts/src/1/1.js
+++ aoc/2015/ts/src/1/1.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
+// floorCount: String -> number
 // Purpose: Produces the floor number based on the instructions
 var floorCount = function (input) {
     if (input.length == 0) {
@@ -17,64 +18,109 @@ var floorCountTests = function () {
         console.log('example passed');
     // Tests
     if (floorCount("(())") == 0) {
-        console.log('example 1 passed');
+        console.log('test 1 passed');
     }
     else {
-        console.log('example 1 failed');
+        console.log('test 1 failed');
     }
     if (floorCount("()()") == 0) {
-        console.log('example 2 passed');
+        console.log('test 2 passed');
     }
     else {
-        console.log('example 2 failed');
+        console.log('test 2 failed');
     }
     if (floorCount("(((") == 3) {
-        console.log('example 3 passed');
+        console.log('test 3 passed');
     }
     else {
-        console.log('example 3 failed');
+        console.log('test 3 failed');
     }
     if (floorCount("(()(()(") == 3) {
-        console.log('example 3 passed');
+        console.log('test 3 passed');
     }
     else {
-        console.log('example 3 failed');
+        console.log('test 3 failed');
     }
     if (floorCount("))(((((") == 3) {
-        console.log('example 4 passed');
+        console.log('test 4 passed');
     }
     else {
-        console.log('example 4 failed');
+        console.log('test 4 failed');
     }
     if (floorCount("())") == -1) {
-        console.log('example 5 passed');
+        console.log('test 5 passed');
     }
     else {
-        console.log('example 5 failed');
+        console.log('test 5 failed');
     }
     if (floorCount("))(") == -1) {
-        console.log('example 6 passed');
+        console.log('test 6 passed');
     }
     else {
-        console.log('example 6 failed');
+        console.log('test 6 failed');
     }
     if (floorCount(")))") == -3) {
-        console.log('example 7 passed');
+        console.log('test 7 passed');
     }
     else {
-        console.log('example 7 failed');
+        console.log('test 7 failed');
     }
     if (floorCount(")())())") == -3) {
-        console.log('example 8 passed');
+        console.log('test 8 passed');
     }
     else {
-        console.log('example 8 failed');
+        console.log('test 8 failed');
     }
 };
-var main = function () {
+// basementCount: String -> number
+// Purpose: Produces the position of the first instruction that instructs Santa to the basement
+var basementCountHelper = function (input, acc, index) {
+    if (input.length == 0) {
+        return new Error("basementCount is not defined on input with length 0");
+    }
+    else {
+        var step = input[0] == '(' ? 1 : -1;
+        if (acc + step < 0) {
+            return index;
+        }
+        else {
+            return basementCountHelper(input.slice(1, input.length), acc + step, index + 1);
+        }
+    }
+};
+var basementCount = function (input) {
+    return basementCountHelper(input, 0, 1);
+};
+var basementCountTests = function () {
+    if (basementCount("") instanceof Error) {
+        console.log("test 9 passed");
+    }
+    else {
+        console.log("test 9 failed");
+    }
+    if (basementCount(")") == 1) {
+        console.log("test 10 passed");
+    }
+    else {
+        console.log("test 10 failed");
+    }
+    if (basementCount("()())") == 5) {
+        console.log("test 11 passed");
+    }
+    else {
+        console.log("test 11 failed");
+    }
+};
+var stars = function () {
     var data = fs.readFileSync("./input", 'utf8');
-    var output = floorCount(data);
-    console.log(output);
+    var outputOne = floorCount(data);
+    var outputTwo = basementCount(data);
+    console.log(outputOne);
+    console.log(outputTwo);
+};
+var main = function () {
+    stars();
+    floorCountTests();
+    basementCountTests();
 };
 main();
-floorCountTests();
--- aoc/2015/ts/src/1/1.ts
+++ aoc/2015/ts/src/1/1.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 
+// floorCount: String -> number
 // Purpose: Produces the floor number based on the instructions
 const floorCount = (input: String): number => {
   if (input.length == 0) {
@@ -16,65 +17,113 @@ const floorCountTests = () => {
 
   // Tests
   if (floorCount("(())") == 0)  {
-    console.log('example 1 passed')
+    console.log('test 1 passed')
   } else {
-    console.log('example 1 failed')
+    console.log('test 1 failed')
   }
 
   if (floorCount("()()") == 0)  {
-    console.log('example 2 passed')
+    console.log('test 2 passed')
   } else {
-    console.log('example 2 failed')
+    console.log('test 2 failed')
   }
 
   if (floorCount("(((") == 3)  {
-    console.log('example 3 passed')
+    console.log('test 3 passed')
   } else {
-    console.log('example 3 failed')
+    console.log('test 3 failed')
   }
 
   if (floorCount("(()(()(") == 3)  {
-    console.log('example 3 passed')
+    console.log('test 3 passed')
   } else {
-    console.log('example 3 failed')
+    console.log('test 3 failed')
   }
 
   if (floorCount("))(((((") == 3)  {
-    console.log('example 4 passed')
+    console.log('test 4 passed')
   } else {
-    console.log('example 4 failed')
+    console.log('test 4 failed')
   }
 
   if (floorCount("())") == -1)  {
-    console.log('example 5 passed')
+    console.log('test 5 passed')
   } else {
-    console.log('example 5 failed')
+    console.log('test 5 failed')
   }
 
   if (floorCount("))(") == -1)  {
-    console.log('example 6 passed')
+    console.log('test 6 passed')
   } else {
-    console.log('example 6 failed')
+    console.log('test 6 failed')
   }
 
   if (floorCount(")))") == -3)  {
-    console.log('example 7 passed')
+    console.log('test 7 passed')
   } else {
-    console.log('example 7 failed')
+    console.log('test 7 failed')
   }
 
   if (floorCount(")())())") == -3)  {
-    console.log('example 8 passed')
+    console.log('test 8 passed')
   } else {
-    console.log('example 8 failed')
+    console.log('test 8 failed')
   }
 }
 
-const main = () => {
+// basementCount: String -> number
+// Purpose: Produces the position of the first instruction that instructs Santa to the basement
+const basementCountHelper = (input: String, acc: number, index: number): number | Error => {
+  if (input.length == 0) {
+    return new Error("basementCount is not defined on input with length 0")
+  } else {
+    const step = input[0] == '(' ? 1 : -1
+    if (acc + step < 0) {
+      return index
+    } else {
+      return basementCountHelper(input.slice(1, input.length), acc + step, index + 1)
+    }
+  }
+}
+
+const basementCount = (input: String): number | Error => {
+  return basementCountHelper(input, 0, 1)
+}
+
+const basementCountTests = () => {
+  if (basementCount("") instanceof Error) {
+    console.log("test 9 passed")
+  } else {
+    console.log("test 9 failed")
+  }
+
+  if (basementCount(")") == 1) {
+    console.log("test 10 passed")
+  } else {
+    console.log("test 10 failed")
+  }
+
+  if (basementCount("()())") == 5) {
+    console.log("test 11 passed")
+  } else {
+    console.log("test 11 failed")
+  }
+}
+
+const stars = () => {
   const data = fs.readFileSync("./input", 'utf8');
-  const output = floorCount(data)
-  console.log(output)
+  const outputOne = floorCount(data)
+  const outputTwo = basementCount(data)
+
+  console.log(outputOne)
+  console.log(outputTwo)
+}
+
+
+const main = () => {
+  stars()
+  floorCountTests()
+  basementCountTests()
 }
 
 main()
-floorCountTests()
