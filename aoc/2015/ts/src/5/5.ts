ea81be59f1eddf1094889b7996e7a65496241a60
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-12-29 13:20:53 -0500

    Complete AOC 2015 Day 5.2 - Typescript

--- aoc/2015/ts/src/5/5.ts
+++ aoc/2015/ts/src/5/5.ts
@@ -148,13 +148,163 @@ const isNiceTests = () => {
   }
 }
 
+// twoLettersTwice: string -> boolean
+// Purpose: Produces true if consumed string contains two letters twice
+const twoLettersTwiceMap = (input: string): Map<string, number> =>
+  input
+    .split('')
+    .reduce<[Map<string, number>, string]>((p, c) =>
+      [new Map([
+        ...Array.from(p[0].entries()),
+        [`${p[1]}${c}`, (p[0].get(`${p[1]}${c}`) || 0) + 1]]), c]
+      , [new Map<string, number>(), ""])[0]
+
+// overlap: string -> boolean
+// Purpose: produces true if needle overlaps with another instance of itself
+const overlap = (haystack: string, needle: string): boolean =>
+  haystack.includes(needle) ?
+    haystack.indexOf(needle) + 1 === haystack.lastIndexOf(needle) :
+    false
+
+
+// Tests:
+const overlapTests = () => {
+  if (overlap("aaa", "aa")) {
+    console.log('test 16 passed')
+  } else {
+    console.log('test 16 failed')
+  }
+
+  if (!overlap("xyxy", "xy")) {
+    console.log('test 17 passed')
+  } else {
+    console.log('test 17 failed')
+  }
+}
+
+const twoLettersTwiceNoOverlap = (input: string): boolean =>
+  Array.from(twoLettersTwiceMap(input)
+    .entries())
+    .filter(([_, count]) => count >= 2)
+    .map(([needle, _]) => !overlap(input, needle))
+    .reduce((p, c) => p || c, false)
+
+const twoLettersTwiceTest = () => {
+  if (twoLettersTwiceNoOverlap("xyxy")) {
+    console.log('test 18 passed')
+  } else {
+    console.log('test 18 failed')
+  }
+
+  if (twoLettersTwiceNoOverlap("aabcdefgaa")) {
+    console.log('test 19 passed')
+  } else {
+    console.log('test 19 failed')
+  }
+
+  if (!twoLettersTwiceNoOverlap("aaa")) {
+    console.log('test 20 passed')
+  } else {
+    console.log('test 20 failed')
+  }
+
+  if (twoLettersTwiceNoOverlap("aaaxyxy")) {
+    console.log('test 21 passed')
+  } else {
+    console.log('test 21 failed')
+  }
+
+  if (twoLettersTwiceNoOverlap("aaaa")) {
+    console.log('test 22 passed')
+  } else {
+    console.log('test 22 failed')
+  }
+}
+
+// alternating: string -> boolean
+// Purpose: produces true if the string contains a letter with exactly one letter
+//          in between
+const alternating = (input: string): boolean => {
+  const [found, lastAcc] = input
+    .split('')
+    .reduce<[boolean, string]>((p, c) =>
+      [
+        p[0] || (p[1].length === 3 && ((p[1][0] === p[1][2]) && (p[1][0] !== p[1][1]))),
+        p[1].length < 3 ? `${p[1]}${c}` : `${p[1][1]}${p[1][2]}${c}`
+      ]
+    , [false, ""])
+
+    return found || lastAcc.length === 3 && ((lastAcc[0] === lastAcc[2]) && (lastAcc[0] != lastAcc[1]))
+}
+
+// Tests:
+const alternatingTests = () => {
+  if (alternating("aba")) {
+    console.log('test 23 passed')
+  } else {
+    console.log('test 23 failed')
+  }
+
+  if (alternating("efe")) {
+    console.log('test 24 passed')
+  } else {
+    console.log('test 24 failed')
+  }
+
+  if (!alternating("abb")) {
+    console.log('test 25 passed')
+  } else {
+    console.log('test 25 failed')
+  }
+
+  if (alternating("abaaaa")) {
+    console.log('test 26 passed')
+  } else {
+    console.log('test 26 failed')
+  }
+}
+
+const isNiceImproved = (input: string): boolean =>
+  twoLettersTwiceNoOverlap(input) && alternating(input)
+
+const isNiceImprovedTests = () => {
+  if (isNiceImproved("qjhvhtzxzqqjkmpb")) {
+    console.log('test 27 passed')
+  } else {
+    console.log('test 27 failed')
+  }
+
+  if (isNiceImproved("xxyxx")) {
+    console.log('test 28 passed')
+  } else {
+    console.log('test 28 failed')
+  }
+  
+  if (!isNiceImproved("uurcxstgmygtbstg")) {
+    console.log('test 29 passed')
+  } else {
+    console.log('test 29 failed')
+  }
+
+  if (!isNiceImproved("ieodomkazucvgmuy")) {
+    console.log('test 30 passed')
+  } else {
+    console.log('test 30 failed')
+  }
+}
+
 
 const niceCount = (input: Array<string>): number => input.filter(s => isNice(s)).length
+const niceImprovedCount = (input: Array<string>): number => input.filter(s => isNiceImproved(s)).length
+
 
 const stars = () => {
   const input = fs.readFileSync('./input', 'utf-8').split('\n')
-  const output = niceCount(input)
-  console.log(output)
+  const outputOne = niceCount(input)
+  const outputTwo = niceImprovedCount(input)
+
+  console.log(outputOne)
+  console.log(outputTwo)
 }
 
 const tests = () => {
@@ -162,6 +312,11 @@ const tests = () => {
   twiceTests()
   threeVowelsTests()
   isNiceTests()
+
+  overlapTests()
+  twoLettersTwiceTest()
+  alternatingTests()
+  isNiceImprovedTests()
 }
 
 const main = () => {
