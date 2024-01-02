b787c25d4299b3b9617fbb2e00181a0060b4dbe0
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-01-02 14:28:13 -0500

    Complete Exercise 5, 6 in OCaml 99 Problems

--- ocaml-99/list.ml
+++ ocaml-99/list.ml
@@ -1,19 +1,29 @@
-let rec last = function
+let rec last l =
+  match l with
   | [] -> None
   | [x] -> Some x
   | _ :: r -> last r
 
-let rec last_pair = function
-  | [] -> None
-  | [x; y] -> Some (x, y)
-  | _ :: r -> last_pair r
-
-let rec nth l x =
+let rec last_two l =
   match l with
   | [] -> None
-  | f :: r -> if x = 0 then Some f else nth r (x-1)
+  | [_] -> None
+  | [x; y] -> Some(x, y)
+  | _ :: r -> last_two r
+
+let rec nth l n =
+  match l with
+  | [] -> None
+  | f :: r -> if n = 0 then Some f else nth r (n-1)
 
 let rec length l =
   match l with
   | [] -> 0
   | _ :: r -> 1 + length r
+
+let rec rev l =
+  match l with
+  | [] -> []
+  | f :: r -> (rev r) @ [f]
+
+let is_palindrome l = l = rev l
--- ocaml-99/test.ml
+++ ocaml-99/test.ml
@@ -4,12 +4,16 @@ open List
 let tests = "test suite for sum" >::: [
   "1.1" >:: (fun _ -> assert_equal (Some "d") (last ["a" ; "b" ; "c" ; "d"]));
   "1.2" >:: (fun _ -> assert_equal None (last []));
-  "2.1" >:: (fun _ -> assert_equal (Some ("c", "d")) (last_pair ["a" ; "b" ; "c" ; "d"]));
-  "2.2" >:: (fun _ -> assert_equal None (last_pair ["a"]));
+  "2.1" >:: (fun _ -> assert_equal (Some ("c", "d")) (last_two ["a" ; "b" ; "c" ; "d"]));
+  "2.2" >:: (fun _ -> assert_equal None (last_two ["a"]));
   "3.1" >:: (fun _ -> assert_equal (Some 3) (nth [1; 2; 3; 4] 2));
-  "3.1" >:: (fun _ -> assert_equal None (nth [] 2));
-  "4.1" >:: (fun _ -> assert_equal 3 (length [1; 2; 3;]));
-  "4.1" >:: (fun _ -> assert_equal 0 (length []))
-
+  "3.2" >:: (fun _ -> assert_equal None (nth [] 2));
+  "4.1" >:: (fun _ -> assert_equal 3 (length [1; 2; 3]));
+  "4.2" >:: (fun _ -> assert_equal 0 (length []));
+  "5.1" >:: (fun _ -> assert_equal [3; 2; 1] (rev [1; 2; 3]));
+  "5.2" >:: (fun _ -> assert_equal [] (rev []));
+  "6.1" >:: (fun _ -> assert_equal true (is_palindrome []));
+  "6.2" >:: (fun _ -> assert_equal true (is_palindrome [1; 2; 3; 2; 1]));
+  "6.3" >:: (fun _ -> assert_equal false (is_palindrome [1; 2; 3]));
 ]
 let _ = run_test_tt_main tests
